7154aa836e68d02d4fbd60ba2b8677ed7aee30c0
James Stone <github@jamesst.one>
2024-04-12 10:24:03 +0200

    add debugging

--- run.sh
+++ run.sh
@@ -100,10 +100,12 @@ function ensureHaveAllSinceDate() {
 
 function addAll() {
      find "$downloadDir" -name '*.json' -exec jq .[] -c {} \; |
-       jq -s |
+       jq -s | tee all.json |
        sql-utils insert "$db" "listens" - \
         --flatten \
         --alter
+
+    cat all.json
 }
 
 makeDB() {
